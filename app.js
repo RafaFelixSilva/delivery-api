3367c30201895019a21c7659317bd2f3e5309c6f
Bruno Cavalcanti <bruno.cavalcanti@jadlog.com.br>
2025-06-27 16:48:20 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+function cadastrado() {
+    let nome = "Bruno"
+    let sobrenome = "Cavalcanti"
+    let nomeCompleto = nome + " " + sobrenome 
+    
+
+    console.log(nomeCompleto);
+    
+}
+
+cadastrado();
