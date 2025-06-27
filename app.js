59b68faad358255778db108cb1f57d677fdb6650
Bruno Cavalcanti <bruno.cavalcanti@jadlog.com.br>
2025-06-27 17:08:25 -0300

    inclusao do numero de celular

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ function cadastrado() {
     let nome = "Bruno"
     let sobrenome = "Cavalcanti"
     let nomeCompleto = nome + " " + sobrenome 
-    
-
-
+    let celular = "(11) 940501957"
     let contato = "(11) 940501957"
 
     console.log(nomeCompleto);
